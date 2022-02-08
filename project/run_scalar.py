8e888765584fade6a4f4b574559d55645ed21a5d
robin-ranabhat <robin.ranabhat@take2games.com>
2022-02-08 18:51:23 +0545

    Task 1.5 : sort of working version implemented

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -10,7 +10,9 @@ class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError('Need to implement for Task 1.5')
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers, 1)
 
     def forward(self, x):
         middle = [h.relu() for h in self.layer1.forward(x)]
@@ -40,7 +42,11 @@ class Linear(minitorch.Module):
 
     def forward(self, inputs):
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError('Need to implement for Task 1.5')
+        y = [ b.value for b in self.bias ]
+        for i, x in enumerate(inputs):
+            for j in range(len(y)):
+                y[j] = y[j] + x * self.weights[i][j].value
+        return y
 
 
 def default_log_fn(epoch, total_loss, correct, losses):
@@ -99,8 +105,15 @@ class ScalarTrain:
 
 
 if __name__ == "__main__":
+    # PTS = 50
+    # HIDDEN = 2
+    # RATE = 0.01
+    # data = minitorch.datasets["Simple"](PTS)
+    # ScalarTrain(HIDDEN).train(data, RATE)
+
     PTS = 50
-    HIDDEN = 2
+    data = minitorch.datasets["Xor"](PTS)
+    HIDDEN = 3
     RATE = 0.5
-    data = minitorch.datasets["Simple"](PTS)
     ScalarTrain(HIDDEN).train(data, RATE)
+
